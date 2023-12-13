8016206bbfcef37f73f5025f9a19a1650335ecbf
rusted-love <32598874+rusted-love@users.noreply.github.com>
2023-12-13 22:50:24 +0200

    Testing Ci validtity (#23)

--- src/restoreScroll.ts
+++ src/restoreScroll.ts
@@ -1,11 +1,9 @@
 import {ScrollPos} from "./types"
 
-
 export const restoreScroll = ([left,top]:ScrollPos) => {
     window.scroll({
         behavior: 'instant',
         left,
         top
     })
-    console.log(`Scroll restored to ${left} ${top}`)
 }
