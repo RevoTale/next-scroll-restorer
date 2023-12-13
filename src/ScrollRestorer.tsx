519d0551f576fb2a86cf1e03117f407661a91e4d
Liakhovskyi Vladyslav <vlad@revotale.com>
2023-12-13 20:23:27 +0200

    Extract scroll restoration as a hook.

--- src/ScrollRestorer.tsx
+++ src/ScrollRestorer.tsx
@@ -1,73 +1,8 @@
 'use client'
-import {usePathname, useSearchParams} from "next/navigation"
-import {useEffect, useRef, useState} from "react"
-import {restoreScroll} from "./restoreScroll"
-import {getScroll, setScroll} from "./storage"
-import {ScrollPos} from "./types"
+import useScrollRestorer from "./useScrollRestorer";
 
-
-const getWindowScroll = (): ScrollPos => [window.scrollX, window.scrollY]
 const ScrollRestoration = () => {
-    const pathname = usePathname()
-
-    const params = useSearchParams()
-    const hash = useRef('')
-    hash.current = `${pathname}?${params.toString()}`
-    const isPopState = useRef(false)
-    const skipNextZero = useRef(false)
-    const [restoreWorkaround,setRestoreWorkaround] = useState<ScrollPos|null>(null)
-    useEffect(() => {
-        if (restoreWorkaround) {
-            restoreScroll(restoreWorkaround)
-        }
-    }, [restoreWorkaround])//This is important because Next.js app dir currently does not respect lifecycle semantics
-    useEffect(() => {
-        window.history.scrollRestoration = 'manual'
-
-        const hash = `${pathname}?${params.toString()}`
-        const existingScroll = getScroll(hash)??[0,0]
-        if (isPopState.current) {
-            isPopState.current = false
-            skipNextZero.current = true
-            if (null !== existingScroll) {
-                restoreScroll(existingScroll)
-                setRestoreWorkaround(existingScroll)
-            }
-        }
-    }, [params, pathname])
-    useEffect(() => {
-        const listener = () => {
-            isPopState.current = true
-        }
-
-        window.addEventListener('popstate', listener,{
-            passive:false
-        })
-        return () => {
-            window.removeEventListener('popstate', listener)
-        }
-    }, [])
-
-    useEffect(() => {
-
-        const listener =  ()=> {
-            const scroll = getWindowScroll()
-            const [x, y] = scroll
-
-            if (skipNextZero.current && x === 0 && y === 0) {
-                skipNextZero.current = false
-                return
-            }
-            setScroll(hash.current, scroll)
-        }
-
-        window.addEventListener('scroll', listener, {
-            passive: false//This is IMPORTANT because passive listener does not respect synchronization and remembers the wrong state
-        })
-        return () => {
-            window.removeEventListener('scroll', listener)
-        }
-    }, [params, pathname])
+    useScrollRestorer()
     return <></>
 }
 export default ScrollRestoration
