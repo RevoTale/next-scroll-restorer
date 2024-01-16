7f9f9a6ed03fda1cde2030eaf7447180c9874efe
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-16 18:07:36 +0200

    End-to-end testing of ScrollRestorer

---
+++ .changeset/rich-apricots-refuse.md
@@ -0,0 +1,5 @@
+---
+"next-scroll-restorer": minor
+---
+
+Add end-to-end testing of `ScrollRestorer` in real NextJS application.
--- __tests__/ScrollRestorer.spec.ts
+++ __tests__/ScrollRestorer.spec.ts
@@ -4,11 +4,27 @@ test('End to end testing of scroll restorer', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('/')
   // Find an element with the text 'About' and click on it
-  await page.getByText('Lets-go to low-page').click()
+  const el = page.getByText('Lets-go to low-page')
+  const getScrollY = ()=> page.evaluate(():number => window.scrollY);
+
+  expect(await getScrollY()).toEqual(0)
+  await el.scrollIntoViewIfNeeded()
+  expect(await getScrollY()).toBeGreaterThan(2599)
+  await el.click()
+
   // The new URL should be "/about" (baseURL is used there)
   await expect(page).toHaveURL('/low-page')
   // The new page should contain an h1 with "About"
   await expect(page.locator('h1')).toContainText('This is the low page')
+  expect(await getScrollY()).toEqual(0)
+
+  await page.goBack()
+  await expect(page).toHaveURL('/')
+
+  expect(await getScrollY()).toBeGreaterThan(2599)
+ await page.goForward()
+
+  expect(await getScrollY()).toEqual(0)
 
 
 })
