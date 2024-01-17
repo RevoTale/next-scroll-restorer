d0727f81620138180dc96ce8ee1b24b69eadcaf6
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-18 00:42:42 +0200

    Extract methods outside of hook. Place console logs to track each inch of scroll restorer behaviour.

--- src/restoreScroll.ts
+++
@@ -1,9 +0,0 @@
-import {ScrollPos} from "./types"
-
-export const restoreScroll = ([left,top]:ScrollPos) => {
-    window.scroll({
-        behavior: 'instant',
-        left,
-        top
-    })
-}
