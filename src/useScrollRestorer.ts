f0708bb986a88be248332c2f7b152d902d85fca9
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-18 00:42:42 +0200

    Extract methods outside of hook. Place console logs to track each inch of scroll restorer behaviour.

--- src/useScrollRestorer.ts
+++ src/useScrollRestorer.ts
@@ -1,27 +1,55 @@
 import {useEffect} from "react"
 import getWindowScroll from "./getWindowScroll"
-import {restoreScroll} from "./restoreScroll"
 import {getCurrentScrollHistory, HistoryState, setCurrentScrollHistory} from "./storage"
+import {ScrollPos} from "./types"
 
+const restoreScroll = ([left,top]:ScrollPos) => {
+    console.log(`Scroll restored to ${left} ${top}.`)
+    window.scroll({
+        behavior: 'instant',
+        left,
+        top
+    })
+}
+
+const rememberScroll = () => {
+    const scroll = getWindowScroll()
+    setCurrentScrollHistory(window.history.state as HistoryState, scroll)
+}
+const mountScroll = () => {
+    console.log('Scroll listener mounted.')
+    window.addEventListener('scroll', rememberScroll)
+}
+const unmountScroll = () => {
+    console.log('Scroll listener unmounted.')
+    window.removeEventListener('scroll', rememberScroll)
+
+}
+const popstate = () => {
+    console.log('Popstate started.')
+    const scroll = getCurrentScrollHistory(window.history.state as HistoryState)
+    if (scroll) {
+        restoreScroll(scroll)
+    }
+}
+const unmountPop = ()=>{
+    console.log('Unmount popstate.')
+
+    window.removeEventListener('popstate', popstate)
+}
+const mountPop = ()=>{
+    console.log('Mount popstate.')
+
+    window.addEventListener('popstate', popstate)
+}
 const useScrollRestorer = (): void => {
-
     useEffect(() => {
         window.history.scrollRestoration = 'manual'
-        const listener = () => {
-            const scroll = getWindowScroll()
-            setCurrentScrollHistory(window.history.state as HistoryState, scroll)
-        }
-        const popstate = () => {
-            const scroll = getCurrentScrollHistory(window.history.state as HistoryState)
-            if (scroll) {
-                restoreScroll(scroll)
-            }
-        }
-        window.addEventListener('popstate', popstate)
-        window.addEventListener('scroll', listener)
+        mountPop()
+        mountScroll()
         return () => {
-            window.removeEventListener('popstate', popstate)
-            window.removeEventListener('scroll', listener)
+            unmountPop()
+            unmountScroll()
         }
     }, [])
 }
