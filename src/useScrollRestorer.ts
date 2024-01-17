363454137f7a703b915e4079042489638b01324d
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-18 00:29:55 +0200

    Save scroll into history

--- src/useScrollRestorer.ts
+++ src/useScrollRestorer.ts
@@ -1,43 +1,21 @@
-import {useOnChange} from "@bladl/react-hooks"
-import {useEffect, useState} from "react"
+import {useEffect} from "react"
 import getWindowScroll from "./getWindowScroll"
 import {restoreScroll} from "./restoreScroll"
-import {getScroll, setScroll} from "./storage"
-import {ScrollPos} from "./types"
-import usePageHref from "./usePageHref"
-export type ScrollRestorerOptions = {
-    unsafeDelay?:number
-}
-const getRealHref = () => window.location.href
-const useScrollRestorer = ({unsafeDelay}:ScrollRestorerOptions= {}): void => {
-    const [scrollToRestore, setScrollToRestore] = useState<ScrollPos | null>(null)
-    const appPageHref = usePageHref()
-    useOnChange(() => {
-        if (null !== scrollToRestore) {
+import {getCurrentScrollHistory, HistoryState, setCurrentScrollHistory} from "./storage"
 
-            setScrollToRestore(null)
-            if (unsafeDelay) {
-                const timeout = setTimeout(()=>{
-                    restoreScroll(scrollToRestore)
-                },unsafeDelay)
-                return ()=>{
-                    clearTimeout(timeout)
-                }
-            } 
-                restoreScroll(scrollToRestore)
+const useScrollRestorer = (): void => {
 
-            
-        }
-        return undefined
-    }, appPageHref)//Such a weird construction is important
     useEffect(() => {
         window.history.scrollRestoration = 'manual'
         const listener = () => {
             const scroll = getWindowScroll()
-            setScroll(window.location.href, scroll)
+            setCurrentScrollHistory(window.history.state as HistoryState, scroll)
         }
         const popstate = () => {
-            setScrollToRestore(getScroll(getRealHref()))
+            const scroll = getCurrentScrollHistory(window.history.state as HistoryState)
+            if (scroll) {
+                restoreScroll(scroll)
+            }
         }
         window.addEventListener('popstate', popstate)
         window.addEventListener('scroll', listener)
