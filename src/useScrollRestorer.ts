260e437ebd441e32b3e7c7486473862671c6411c
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-19 22:29:06 +0200

    Prevent error Unhandled Rejection (SecurityError): Attempt to use history.pushState() more than 100 times per 30 seconds react

--- src/useScrollRestorer.ts
+++ src/useScrollRestorer.ts
@@ -1,10 +1,10 @@
-import {useEffect, useLayoutEffect,} from "react"
+import {useEffect, useLayoutEffect, useRef,} from "react"
 import {getScrollFromState, HistoryState, ScrollPos, setCurrentScrollHistory} from "./storage"
 import usePageHref from "./usePageHref"
 
 const browserInfluenceDetectorOffset = 80
 const getWindowScroll = (): ScrollPos => [window.scrollX, window.scrollY]
-
+const memoizationInterval = 300
 const restoreScroll = ([left, top]: ScrollPos) => {
     console.log(`Scroll restored to ${left} ${top}.`)
     window.scroll({
@@ -15,25 +15,6 @@ const restoreScroll = ([left, top]: ScrollPos) => {
 }
 
 
-const popstate = (e: PopStateEvent) => {
-    console.log('Popstate started.')
-    console.log(e.state, window.history.state)
-    const scroll = getScrollFromState(e.state as HistoryState)
-    console.log(`Found scroll ${scroll?.toString()}.`)
-    if (scroll) {
-        restoreScroll(scroll)
-    }
-}
-const unmountPop = () => {
-    console.log('Unmount popstate.')
-
-    window.removeEventListener('popstate', popstate)
-}
-const mountPop = () => {
-    console.log('Mount popstate.')
-
-    window.addEventListener('popstate', popstate)
-}
 const restoreCurrentScroll = () => {
     const scroll = getScrollFromState(window.history.state as HistoryState)
     console.log(`Restoring current scroll position. ${scroll?.toString()}`)
@@ -44,6 +25,7 @@ const restoreCurrentScroll = () => {
 }
 const useScrollRestorer = (): void => {
     const appHref = usePageHref()
+
     useLayoutEffect(() => {
         /**
          * This is important to run as late as possible after navigation.
@@ -54,22 +36,64 @@ const useScrollRestorer = (): void => {
         console.log(`Layout effect ${window.scrollY}`)
         restoreCurrentScroll()
     }, [appHref])
+
+
+    const currentScroll = useRef<ScrollPos>([0, 0])
+    const scrollMemoizationTimeoutRef = useRef<ReturnType<typeof setTimeout>>()
     useEffect(() => {
         window.history.scrollRestoration = 'manual'
+        const popstate = (e: PopStateEvent) => {
+            cancelMemoization()
+            console.log('Popstate started.')
+            const scroll = getScrollFromState(e.state as HistoryState)
+            console.log(`Found scroll ${scroll?.toString()}.`)
+            if (scroll) {
+                restoreScroll(scroll)
+            }
 
-        const scrollListener = () => {
-            const scroll = getWindowScroll()
-            const [x,y] = scroll
+        }
+        const rememberScroll = () => {
+            const [x, y] = currentScroll.current
+            console.log(`Remember scroll ${x} ${y}.`)
+
             if (x === 0 && y === 0) {
                 // Sometimes Safari scroll to the start because of weird behaviour We restore it back.
                 // This case cannot be tested with Playwright, or any other testing library.
-                const [prevX,prevY] = getScrollFromState(window.history.state as HistoryState)??[0,0]
-                if ((prevX>0 || prevY>0) && (prevX>browserInfluenceDetectorOffset || prevY>browserInfluenceDetectorOffset)) {
+                const [prevX, prevY] = getScrollFromState(window.history.state as HistoryState) ?? [0, 0]
+                if ((prevX > 0 || prevY > 0) && (prevX > browserInfluenceDetectorOffset || prevY > browserInfluenceDetectorOffset)) {
                     console.log('Reverting back scroll because browser tried to brake it.')
                     restoreCurrentScroll()
                 }
+
             }
-            setCurrentScrollHistory(scroll)
+            setCurrentScrollHistory([x,y])
+        }
+        const unmountPop = () => {
+            console.log('Unmount popstate.')
+
+            window.removeEventListener('popstate', popstate)
+        }
+        const mountPop = () => {
+            console.log('Mount popstate.')
+
+            window.addEventListener('popstate', popstate)
+        }
+        const cancelMemoization = ()=>{
+            if (scrollMemoizationTimeoutRef.current) {
+                clearTimeout(scrollMemoizationTimeoutRef.current)
+            }
+            scrollMemoizationTimeoutRef.current = undefined
+        }
+        const scrollListener = () => {
+            currentScroll.current = getWindowScroll()
+            cancelMemoization()
+            if (scrollMemoizationTimeoutRef.current === undefined) {
+                rememberScroll()
+            }
+            scrollMemoizationTimeoutRef.current = setTimeout(() => {
+                rememberScroll()
+                scrollMemoizationTimeoutRef.current = undefined
+            }, memoizationInterval)
         }
         const mountScroll = () => {
             console.log('Scroll listener mounted.')
