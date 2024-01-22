3ad22df82025c8f9bbcfe0132aedb86ae7118f4b
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-22 18:54:09 +0200

    Make tests execution one by one.

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,13 +21,13 @@ export default defineConfig({
   },
   testDir: './__tests__',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [ ['html', { open: 'never' }] ],
