8d7237e1604350d4d344deb022e7d191a11241b5
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-01-22 19:36:06 +0200

    Restructure tests to run faster in parallel. All tests are passing.

--- __tests__/ScrollRestorer.spec.ts
+++ __tests__/ScrollRestorer.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect,} from '@playwright/test'
+import {test, expect, Page,} from '@playwright/test'
 
 const highPage = 1300
 const mainPage = 2600
@@ -9,89 +9,88 @@ const resolveTimeout = (time: number) => (async () => {
         }, time)
     })
 })()
-test('End to end testing of scroll restorer', async ({page, browserName}) => {
+const getScrollY = (page:Page) => page.evaluate((): number => window.scrollY)
+
+const expectScrollToBe = async (page:Page,value: number) => {
+    await resolveTimeout(50)
+    await expect(getScrollY(page)).resolves.toBeGreaterThanOrEqual(value - 2)
+    await expect(getScrollY(page)).resolves.toBeLessThanOrEqual(value + 2)
+}
+const initTests = async (page:Page)=>{
     // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
     page.on('console', (msg) => {
         console.log(`${msg.type()}: ${msg.text()}`)
     })
-
     await page.goto('/')
-    // Find an element with the text 'About' and click on it
+    // Default behavior to start.
     const el = page.getByText('Lets-go to low-page')
-    const getScrollY = () => page.evaluate((): number => window.scrollY)
-    const expectScrollToBe = async (value: number) => {
-        await resolveTimeout(50)
-        await expect(getScrollY()).resolves.toBeGreaterThanOrEqual(value - 2)
-        await expect(getScrollY()).resolves.toBeLessThanOrEqual(value + 2)
-    }
-    await expectScrollToBe(0)
+    await expectScrollToBe(page,0)
     await el.scrollIntoViewIfNeeded()
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
     await el.click()
-
-    // The new URL should be "/about" (baseURL is used there)
     await expect(page).toHaveURL('/low-page')
-    // The new page should contain an h1 with "About"
     await expect(page.locator('h1')).toContainText('This is the low page')
-    await expectScrollToBe(0)
+    await expectScrollToBe(page,0)
     await page.goBack()
+}
+test('End to end testing of scroll restorer', async ({page, browserName}) => {
+
+    await initTests(page)
     await expect(page).toHaveURL('/')
     await resolveTimeout(1000) //Check if Next.js does not brake scroll position later
-
-
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
     await page.goForward()
-    await expectScrollToBe(0)
+    await expectScrollToBe(page,0)
     //A little bit of stress for app
     await page.goBack()
 
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
     for (let i = 0; i < 10; i++) {
         await page.goForward()
         await resolveTimeout(10)//Sometimes browsers struggle to restore the same millisecond
         await page.goBack()
     }
 
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
     await page.goForward()
 
-    await expectScrollToBe(0)
+    await expectScrollToBe(page,0)
     await page.goBack()
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
 
 //End of a little bit of stress for app
 
     //Test when both pages are high
     await page.goto('/high')
-    await expectScrollToBe(0)
+    await expectScrollToBe(page,0)
 
     const mainEl = page.getByText('Lets-go to main')
     await mainEl.scrollIntoViewIfNeeded()
     await page.waitForURL('/high')
-    await expectScrollToBe(highPage)
+    await expectScrollToBe(page,highPage)
     await mainEl.click()
-    await expectScrollToBe(0)
+    await expectScrollToBe(page,0)
     await page.waitForURL('/')
 
 
     await page.getByText('Lets-go to low-page').scrollIntoViewIfNeeded()
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
 
     await page.getByText('Lets-go to low-page').click()
 
-    await expectScrollToBe(0)
+    await expectScrollToBe(page,0)
     await page.waitForURL('/low-page')
 
     await page.goBack()
 
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
     await page.waitForURL('/')
     await page.goBack()
-    await expectScrollToBe(highPage)
+    await expectScrollToBe(page,highPage)
     await page.waitForURL('/high')
     await page.goForward()
     await page.waitForURL('/')
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
 
 
     await page.reload()
@@ -99,15 +98,21 @@ test('End to end testing of scroll restorer', async ({page, browserName}) => {
         await page.goBack() //Firefox pushed new history entry history after reload https://github.com/microsoft/playwright/issues/22640
     }
     await resolveTimeout(1000)//Sometimes browsers struggle to restore the same millisecond
-    await expectScrollToBe(mainPage)
+    await expectScrollToBe(page,mainPage)
 
-    //Test for a scroll to not produce any errors. https://github.com/sveltejs/kit/issues/365
+})
+
+test('Smooth scrolling',async ({page}) => {
+    await initTests(page)
+
+    //Test for a scroll to not produce any errors. Https://github.com/sveltejs/kit/issues/365
     let error: string | null = null
     page.on("console", (msg) => {
         if (msg.type() === 'error') {
             error = msg.text()
         }
     })
+
     await resolveTimeout(2000) // Timeout for safari scroll bug navigation workaround.
 
     const smoothScrollTop = async () => {
@@ -128,7 +133,7 @@ test('End to end testing of scroll restorer', async ({page, browserName}) => {
             polling: 500
 
         })
-        await expectScrollToBe(0)
+        await expectScrollToBe(page,0)
     }
     const smoothScrollBottom = async () => {
         console.log('Scrolling to bottom.')
@@ -144,7 +149,7 @@ test('End to end testing of scroll restorer', async ({page, browserName}) => {
             polling: 500
         })
 
-        await expectScrollToBe(mainPage)
+        await expectScrollToBe(page,mainPage)
     }
     //This is a very important test to stress out 'scroll' event
     // DO NOT CHANGE THIS TEST IN ANY WAY!
@@ -155,5 +160,4 @@ test('End to end testing of scroll restorer', async ({page, browserName}) => {
     }
 
     expect(error).toBe(null)
-
 })
